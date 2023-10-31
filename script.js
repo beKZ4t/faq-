b44773c55bb20319caf5ab26be06a63d2bafe38a
Bekzat <utebalievbekzat@gmail.com>
2023-10-31 10:40:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggle(item) {
+//  let answer = item.contains('active');
+  console.log()
+  if (!item.classList.contains('active')) {
+    item.classList.add('active')
+  } else {
+    item.classList.remove('active')
+  }
+}
+
+
